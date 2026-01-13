9d67afffdaab13870d2ce01b3da909f5d5cb065a
Jibesh Pahari <mailjibesh@webgrity.net>
2026-01-13 19:05:21 +0530

    chnage

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -69,6 +69,25 @@
                             <div class="row">
                                 <div class="col-5">
                                     <div class="icon-big text-center">
+                                        <i class="icon-wallet text-success"></i>
+                                    </div>
+                                </div>
+                                <div class="col-7 col-stats">
+                                    <div class="numbers">
+                                        <p class="card-category">Upcoming Session</p>
+                                        <h4 class="card-title">5</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="card card-stats card-round">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-5">
+                                    <div class="icon-big text-center">
                                         <i class="icon-pie-chart text-warning"></i>
                                     </div>
                                 </div>
@@ -95,44 +114,6 @@
                                     <div class="numbers">
                                         <p class="card-category">Hours Learned</p>
                                         <h4 class="card-title">50+</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-md-4">
-                    <div class="card card-stats card-round">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-5">
-                                    <div class="icon-big text-center">
-                                        <i class="icon-wallet text-success"></i>
-                                    </div>
-                                </div>
-                                <div class="col-7 col-stats">
-                                    <div class="numbers">
-                                        <p class="card-category">Upcoming Session</p>
-                                        <h4 class="card-title">5</h4>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-md-3 d-none">
-                    <div class="card card-stats card-round">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-5">
-                                    <div class="icon-big text-center">
-                                        <i class="icon-social-twitter text-primary"></i>
-                                    </div>
-                                </div>
-                                <div class="col-7 col-stats">
-                                    <div class="numbers">
-                                        <p class="card-category">Followers</p>
-                                        <h4 class="card-title">+45K</h4>
                                     </div>
                                 </div>
                             </div>
